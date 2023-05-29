f99bebd93e445152ec35c978314bf2733f49e656
ynwqmv <112755279+ynwqmv@users.noreply.github.com>
2023-05-28 20:53:58 -0700

    init

--- target/package/keygenx-0.1.2/target/debug/deps/keygenx-ef49bddbbd35454c.d
+++
@@ -1,7 +0,0 @@
-C:\Users\qumar\Desktop\keygen\target\package\keygenx-0.1.2\target\debug\deps\keygenx-ef49bddbbd35454c.rmeta: src\lib.rs
-
-C:\Users\qumar\Desktop\keygen\target\package\keygenx-0.1.2\target\debug\deps\libkeygenx-ef49bddbbd35454c.rlib: src\lib.rs
-
-C:\Users\qumar\Desktop\keygen\target\package\keygenx-0.1.2\target\debug\deps\keygenx-ef49bddbbd35454c.d: src\lib.rs
-
-src\lib.rs:
--- target/package/keygenx-0.1.2/target/debug/libkeygenx.d
+++
@@ -1,1 +0,0 @@
-C:\Users\qumar\Desktop\keygen\target\package\keygenx-0.1.2\target\debug\libkeygenx.rlib: C:\Users\qumar\Desktop\keygen\target\package\keygenx-0.1.2\src\lib.rs
