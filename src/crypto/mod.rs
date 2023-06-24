077699e5f5342317a36b9d7517bb8a4336493d7e
Alexander <112755279+alexanderqmv@users.noreply.github.com>
2023-06-24 10:53:21 -0700

    Update mod.rs

--- src/crypto/mod.rs
+++ src/crypto/mod.rs
@@ -1,48 +1,45 @@
-/// [`Hash`] - scope (or module) where is the `Hasher` structure located 
-pub mod Hash {
-    use crypto::{
-        digest::Digest,
-        md5::{self, Md5},
-        sha2::Sha256,
-    };
-    /// [`Hasher`] - hasher structure contains 2 hashing algorith methods (`hash_with_sha256()`, `hash_with_md5()`)
-    pub struct Hasher {}
+use crypto::{
+    digest::Digest,
+    md5::{self, Md5},
+    sha2::Sha256,
+};
+/// [`Hasher`] - hasher structure contains 2 hashing algorith methods (`hash_with_sha256()`, `hash_with_md5()`)
+pub struct Hasher {}
 
-    impl Hasher {
-        pub fn new() -> Self {
-            Hasher {}
+impl Hasher {
+    pub fn new() -> Self {
+        Hasher {}
+    }
+    ///`hash_with_sha256` -  Implements `SHA256` hashing algorithm and returns `String` (Security ⭐⭐⭐⭐⭐)
+
+    /// ## Example
+    /// ```
+    /// fn main() {
+    ///
+    ///     /* snip.. */
+    ///     let mut sha256_hasher = Hasher::new();
+    ///     
+    ///     let res = sha256_hasher.hash_with_sha256(&str);
+    ///     // do some work with `res`, for example print the value
+    ///     println!("{res}");
+    /// }
+    pub fn hash_with_sha256(&self, key: &String) -> Option<String> {
+        if key.len() == 0 {
+            return None;
         }
-        ///`hash_with_sha256` -  Implements `SHA256` hashing algorithm and returns `String` (Security ⭐⭐⭐⭐⭐)
-        
-        /// ## Example
-        /// ```
-        /// fn main() {
-        ///
-        ///     /* snip.. */
-        ///     let mut sha256_hasher = Hasher::new();
-        ///     
-        ///     let res = sha256_hasher.hash_with_sha256(&str);
-        ///     // do some work with `res`, for example print the value
-        ///     println!("{res}"); 
-        /// }
-        pub fn hash_with_sha256(&self, key: &String) -> Option<String> {
-            if key.len() == 0 {
-                None
-            }
-            let mut sha2_hasher = Sha256::new();
-            sha2_hasher.input_str(key);
-            
-            Some(sha2_hasher.result_str())
+        let mut sha2_hasher = Sha256::new();
+        sha2_hasher.input_str(key);
+
+        Some(sha2_hasher.result_str())
+    }
+    /// Implements `MD5` hashing algorithm and returns `String` (Security ⭐⭐⭐⭐)
+    pub fn hash_with_md5(&self, key: &String) -> Option<String> {
+        if key.len() == 0 {
+            return None;
         }
-        /// Implements `MD5` hashing algorithm and returns `String` (Security ⭐⭐⭐⭐)
-        pub fn hash_with_md5(&self, key: &String) -> Option<String> {
-            if key.len() == 0 {
-                None
-            }
-            let mut md5_hasher = Md5::new();
-            md5_hasher.input_str(key);
-            
-            Some(md5_hasher.result_str())
-        }
+        let mut md5_hasher = Md5::new();
+        md5_hasher.input_str(key);
+
+        Some(md5_hasher.result_str())
     }
 }
